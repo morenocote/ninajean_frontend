4765ee8669b5e5993e01708351383adc3b0eaa5b
jairo rubiel moreno cote <morenocotej@gmail.com>
2026-01-20 14:42:08 -0500

    act url server

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = import.meta.env.VITE_API_URL || 'http://192.168.1.13:5000/api';
+export const API_URL = import.meta.env.VITE_API_URL || 'https://salmon-narwhal-683642.hostingersite.com/api';
 export const BASE_URL = API_URL.replace('/api', '');
